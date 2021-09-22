8627124a011fcc41d9aa1ce13083bff859c494f3
romy1992 <trottarosario@gmail.com>
2021-09-22 15:22:16 +0200

    Delete java/com/commos/roles directory

--- java/com/commos/roles/resource/FNRConfirmedEmail.java
+++
@@ -1,8 +0,0 @@
-package com.commos.roles.resource;
-
-import org.springframework.web.bind.annotation.RequestParam;
-
-public interface FNRConfirmedEmail {
-
-  String confirmedEmail(@RequestParam("email") String email, @RequestParam("token") String token);
-}
